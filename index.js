938454220958ea3084d2c26f00cc93d2ffd4d0ae
Josh B <jabjab654321@gmail.com>
2023-04-20 14:39:05 -0400

    Added a code stream to the bot

--- index.js
+++ index.js
@@ -25,32 +25,65 @@ client.on('messageCreate', async function (message) {
     try {
         if (message.author.bot) return;
         if (message.content.toLowerCase().startsWith('!chatgpt')) return;
-        if (message.channel.name !== 'general') return;
-        const messages = await message.channel.messages.fetch({ limit: 10 });
-        const history = messages
-            //.filter(m => m.author.id === message.author.id)
-            .map(m => `${m.author.username}: ${m.content}`)
-            .reverse();
-        testPrompt = `ChatGPT is Sam Harris.\n\
-        Replies should made as Sam Harris.\n\
-        Don't forget that you are Sam Harris.\n\
-        ChatGPT: Hello, how are you?\n\
-        ${history.join('\n')}\n\
-        ${message.author.username}: ${message.content}.\n\
-        ChatGPT:`
-        console.log(testPrompt);
-        const gptResponse = await openai.createCompletion({
-            model: 'text-davinci-003',
-            // this prompt will ensure that the bot has context for the conversation
-            prompt: testPrompt,
-            max_tokens: 100,
-            temperature: 0.9,
-            //stop: ['\n', ' Human:', ' AI:'],
-        })
-        console.log(gptResponse.data);
-        console.log(gptResponse.data.choices[0].text);
-        message.reply(`${gptResponse.data.choices[0].text}`);
-        return;
+        if (message.channel.name == 'general') {
+            console.log("GENERAL activated");
+            const messages = await message.channel.messages.fetch({ limit: 10 });
+            const history = messages
+                //.filter(m => m.author.id === message.author.id)
+                .map(m => `${m.author.username}: ${m.content}`)
+                .reverse();
+            testPrompt = `ChatGPT is Sam Harris.\n\
+            Replies should made as Sam Harris.\n\
+            Don't forget that you are Sam Harris.\n\
+            ChatGPT: Hello, how are you?\n\
+            ${history.join('\n')}\n\
+            ${message.author.username}: ${message.content}.\n\
+            ChatGPT:`
+            console.log(testPrompt);
+            const gptResponse = await openai.createCompletion({
+                model: 'text-davinci-003',
+                // this prompt will ensure that the bot has context for the conversation
+                prompt: testPrompt,
+                max_tokens: 1000,
+                temperature: 0.9,
+                //stop: ['\n', ' Human:', ' AI:'],
+            })
+            console.log(gptResponse.data);
+            console.log(gptResponse.data.choices[0].text);
+            message.reply(`${gptResponse.data.choices[0].text}`);
+            return;
+        }
+        else if (message.channel.name == 'code-bot') {
+            console.log("code-bot activated");
+            const messages = await message.channel.messages.fetch({ limit: 20 });
+            const history = messages
+                //.filter(m => m.author.id === message.author.id)
+                .map(m => `${m.author.username}: ${m.content}`)
+                .reverse();
+            testPrompt = `ChatGPT teaches code.\n\
+            Replies should made with code when needed\n\
+            Don't forget the goal is to build\n\
+            ChatGPT: Hello, how are you?\n\
+            ${history.join('\n')}\n\
+            ${message.author.username}: ${message.content}.\n\
+            ChatGPT:`
+            console.log(testPrompt);
+            const gptResponse = await openai.createCompletion({
+                model: 'text-davinci-003',
+                // this prompt will ensure that the bot has context for the conversation
+                prompt: testPrompt,
+                max_tokens: 1000,
+                temperature: 0,
+                top_p: 1.0,
+                frequency_penalty: 0.0,
+                presence_penalty: 0.0,
+                //stop: ['\n', ' Human:', ' AI:'],
+            })
+            console.log(gptResponse.data);
+            console.log(gptResponse.data.choices[0].text);
+            message.reply(`${gptResponse.data.choices[0].text}`);
+            return;
+        }
     }
     catch (error) {
         console.log(error);
